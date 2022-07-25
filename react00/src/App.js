134fd93ab978110a1a0d7dbeda30b8c2d0d74df0
mokdanbi <l26tempo@naver.com>
2022-07-25 12:21:38 +0900

    0725

---
+++ react00/src/App.js
@@ -0,0 +1,17 @@
+import Test from "./test";
+import Nav from "./Nav";
+
+const App = () => {
+  // console.log('콘솔')
+  return (
+    <>
+      <Test />
+      <h1>h1 tag</h1>
+      <Nav />
+      <Test />
+      {/* <h2>h2 tag</h2> */}
+    </>
+  )
+}
+
+export default App;
---
+++ react00/src/test.js
@@ -0,0 +1,11 @@
+function Test() {
+    return (
+        <>
+            <i className="xi-bars"></i>
+            <p>Lorem ipsum dolor sit.</p>
+        </>
+    )
+}
+
+export default Test;
+// 맨 앞 대문자로 쓰는게 원칙
